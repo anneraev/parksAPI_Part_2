c203a065a7a4a9a5c60e8d6277ab07ee26340343
anneraev <47757104+anneraev@users.noreply.github.com>
2019-03-21 12:07:50 -0500

    fixed problem with apiManager, refactored code working again.

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,9 +1,7 @@
 const apiBaseUrl = "http://localhost:8088"
 
-//gets all items in the specified api directory. The passed argument is the name of the directory.
 const getAllItems = (directoryName) => fetch(`${apiBaseUrl}/${directoryName}`).then(response => response.json())
 
-//delete's specified item from specified api directory. The first argument passed is the name of the directory, the second is the ID of the item to delete.
-const deleteItem = (directoryName, parkId) => fetch(`${apiBaseUrl}/${directoryName}/${parkId}`, {
-    method: "DELETE"
+const deleteItems = (directoryName, parkId) => fetch(`${apiBaseUrl}/${directoryName}/${parkId}`, {
+  method: "DELETE"
 })
